3d47345c03a07a5da6b1b7d61b72f38faa31e10c
sshurtado <s.shurtado@alumnos.upm.es>
2024-07-18 14:41:00 +0100

    Metrics notebook completed, tested and cleaned up

--- swdf/metrics.py
+++ swdf/metrics.py
@@ -3,12 +3,13 @@
 # %% auto 0
 __all__ = ['Metrics', 'RegressiveMetrics', 'SOLFMYMetrics', 'GEODSTAPMetrics', 'ClassificationMetrics',
            'SOLFMYClassificationMetrics', 'GEODSTAPClassificationMetrics', 'LossMetrics', 'OutlierDetectionMetrics',
-           'AUPRCMetric', 'R2Score']
+           'F1ScoreMetrics', 'AUPRCMetric', 'KSDifferenceMetric', 'AssociationMetrics', 'AccuracyMetrics',
+           'BiasMetrics']
 
 # %% ../nbs/metrics.ipynb 0
 import sys
 sys.path.append('..')
-from abc import ABC, abstractmethod
+from abc import ABC
 import torch
 import numpy as np
 import pandas as pd
@@ -309,7 +310,7 @@ class LossMetrics(Metrics):
         
         return [self.Metrics_Not_Available]
 
-# %% ../nbs/metrics.ipynb 21
+# %% ../nbs/metrics.ipynb 22
 class OutlierDetectionMetrics(Metrics):
     def __init__(self, threshold=3.5):
         super().__init__()
@@ -348,18 +349,127 @@ class OutlierDetectionMetrics(Metrics):
         z_scores = self._modified_z_score(values)
         return torch.abs(z_scores) > self.threshold
     
+    def _evaluate_outlier_predicted(self, y_true, y_pred):
+        """
+        Evaluate the performance of outlier detection.
+        
+        Parameters:
+        y_true (torch.Tensor): Actual values tensor of shape (batch_size, variables, horizon)
+        y_pred (torch.Tensor): Predicted values tensor of the same shape as y_true
+        
+        Returns:
+        AttrDict: Dictionary with true/false positives, false negatives, indices of true/predicted outliers
+        """    
+        # Detect outliers based on the threshold
+        true_outliers = self._detect_outliers(y_true)
+        pred_outliers = self._detect_outliers(y_pred)
+        
+        # Evaluate the detection by comparing true outliers and predicted outliers
+        tp = torch.sum((pred_outliers & true_outliers).float())  # True Positives
+        fp = torch.sum((pred_outliers & ~true_outliers).float()) # False Positives
+        fn = torch.sum((~pred_outliers & true_outliers).float()) # False Negatives
+        tn = torch.sum((~pred_outliers & ~true_outliers).float()) # True Negatives
+
+        return AttrDict({
+            "tp": tp,
+            "fp": fp,
+            "fn": fn,
+            "tn": tn,
+            "true_outliers": true_outliers,
+            "predicted_outliers": pred_outliers
+        })
+    
+    
     @abstractmethod
     def get_metrics(self) -> list:
         return NotImplementedError
 
-
+# %% ../nbs/metrics.ipynb 24
+class F1ScoreMetrics(OutlierDetectionMetrics):
+    def __init__(self, threshold=3.5, metrics='F1_Score'):
+        super().__init__(threshold)
+        self.metrics = metrics
     
 
-# %% ../nbs/metrics.ipynb 25
+    # Metrics
+    def Precision(self, y_true, y_pred):
+        stats = self._evaluate_outlier_predicted(y_true, y_pred)
+
+        # To avoid divide by 0
+        if (stats.tp + stats.fp) > 0:
+            precision = stats.tp / (stats.tp + stats.fp)  
+        else: 
+            precision = torch.tensor(0.0)
+
+        return precision
+    
+    def Recall(self, y_true, y_pred):
+        stats = self._evaluate_outlier_predicted(y_true, y_pred)
+
+        if (stats.tp + stats.fn) > 0:
+            recall = stats.tp / (stats.tp + stats.fn)
+        else: 
+            recall = torch.tensor(0.0)
+
+        return recall
+    
+    def F1_Score(self, y_true, y_pred):
+        precision = self.Precision(y_true, y_pred)
+        recall = self.Recall(y_true, y_pred)
+
+        if (precision + recall) > 0:
+            f1_score = 2 * (precision * recall) / (precision + recall)
+        else: 
+            f1_score = torch.tensor(0.0)
+
+        return f1_score
+    
+    def Accuracy_Score(self, y_true, y_pred):
+        stats = self._evaluate_outlier_predicted(y_true, y_pred)
+        
+        if (stats.tp + stats.fp + stats.fn + stats.tn) > 0:
+            return (stats.tp + stats.tn) / (stats.tp + stats.fp + stats.fn + stats.tn)
+        else:
+            return torch.tensor(0.0)
+    
+    def Specificity(self, y_true, y_pred):
+        stats = self._evaluate_outlier_predicted(y_true, y_pred)
+        
+        if (stats.tn + stats.fp) > 0:
+            return stats.tn / (stats.tn + stats.fp)
+        else:
+            return torch.tensor(0.0)
+
+    def Negative_Predictive_Value(self, y_true, y_pred):
+        stats = self._evaluate_outlier_predicted(y_true, y_pred)
+
+        if (stats.tn + stats.fn) > 0:
+            return stats.tn / (stats.tn + stats.fn)
+        else:
+            return torch.tensor(0.0)
+    
+    def Δ_Detected_Outliers (self, y_true, y_pred):
+        stats = self._evaluate_outlier_predicted(y_true, y_pred)
+        
+        return torch.sum(stats.true_outliers & ~stats.predicted_outliers)
+
+
+    # Metrics retrieval function
+    def get_metrics(self) -> list:
+        if self.metrics == 'F1_Score':
+            return [self.F1_Score]
+        elif self.metrics == 'All':
+            return [self.Precision, self.Recall, self.F1_Score, self.Accuracy_Score, self.Specificity, self.Negative_Predictive_Value, self.Δ_Detected_Outliers ]
+        else:
+            return [self.Precision, self.Recall, self.F1_Score, self.Δ_Detected_Outliers ]
+
+# %% ../nbs/metrics.ipynb 26
 class AUPRCMetric(OutlierDetectionMetrics):
     def __init__(self, threshold=3.5):
-        super().__init__(threshold=threshold)
+        super().__init__(threshold)
 
+
+    # Metrics
     def AURPC(self, y_true, y_pred):
         """
         Calculate the Area Under the Precision-Recall Curve (AUPRC).
@@ -379,21 +489,85 @@ class AUPRCMetric(OutlierDetectionMetrics):
         # Use precision_recall_curve to get precision and recall for different thresholds
         precision, recall, _ = precision_recall_curve(true_outliers_flat, pred_z_scores_flat)
         
-        print(type(precision), type(recall))
         auprc_value = auc(recall, precision)
         
         return torch.tensor(auprc_value, device=y_true.device)
+    
 
+    # Metrics retrieval function
     def get_metrics(self) -> list:
         return [self.AURPC]
 
-# %% ../nbs/metrics.ipynb 27
-# TODO: Revisar
-class R2Score(Metrics):
+# %% ../nbs/metrics.ipynb 28
+class KSDifferenceMetric(Metrics):
+    def __init__(self, threshold=3.5):
+        super().__init__()
+        self.threshold = threshold
+
+    @staticmethod
+    def skewness(x):
+        mean = torch.mean(x)
+        std_dev = torch.std(x, unbiased=False)
+        
+        skewness = torch.mean(((x - mean) / std_dev) ** 3)
+        return skewness
+
+    @staticmethod
+    def kurtosis(x):
+        mean = torch.mean(x)
+        std_dev = torch.std(x, unbiased=False)
+        
+        kurtosis = torch.mean(((x - mean) / std_dev) ** 4)
+        return kurtosis
+    
+
+    # Metrics
+    def Δ_Skewness(self, y_true, y_pred):
+        true_skewness = KSDifferenceMetric.skewness(y_true)
+        pred_skewness = KSDifferenceMetric.skewness(y_pred)
+        
+        return torch.abs(true_skewness - pred_skewness)
+    
+    def Δ_Kurtosis(self, y_true, y_pred):
+        true_kurtosis = KSDifferenceMetric.kurtosis(y_true)
+        pred_kurtosis = KSDifferenceMetric.kurtosis(y_pred)
+        
+        return torch.abs(true_kurtosis - pred_kurtosis)
+        
+
+    # Metrics retrieval function
+    def get_metrics(self) -> list:
+        return [self.Δ_Skewness, self.Δ_Kurtosis]
+
+# %% ../nbs/metrics.ipynb 31
+class AssociationMetrics(Metrics):
     def __init__(self):
         super().__init__()
 
-    def R2(self, y_true, y_pred):
+    # Metrics
+    def R_Correlation(self, y_true, y_pred):
+        """
+        Calculate the Pearson Correlation Coefficient (R Correlation) between true and predicted values.
+        
+        Parameters:
+        y_true (torch.Tensor): Actual values tensor of shape (batch_size, variables, horizon)
+        y_pred (torch.Tensor): Predicted values tensor of the same shape as y_true
+        
+        Returns:
+        torch.Tensor: R Correlation coefficient
+        """
+        y_true_flat = y_true.view(-1)
+        y_pred_flat = y_pred.view(-1)
+        
+        # To be able to use torch.corrcoef, we need to stack the tensors
+        stacked = torch.stack([y_true_flat, y_pred_flat])
+        
+        corr_matrix = torch.corrcoef(stacked)
+        
+        r_value = corr_matrix[0, 1]
+        return r_value
+
+    def R2_Score(self, y_true, y_pred):
         """
         Calculate the R^2 score.
         
@@ -404,21 +578,100 @@ class R2Score(Metrics):
         Returns:
         torch.Tensor: R^2 score
         """
-        # Calculate the mean of the actual values
         y_true_mean = torch.mean(y_true, dim=2, keepdim=True)
+        print(y_true_mean.shape, y_true.shape)
         
-        # Calculate the total sum of squares
+        # Total Sum of Squares
         ss_tot = torch.sum((y_true - y_true_mean) ** 2)
         
-        # Calculate the residual sum of squares
+        # Residual Sum of Squares
         ss_res = torch.sum((y_true - y_pred) ** 2)
         
-        # Calculate the R^2 score
         r2 = 1 - ss_res / ss_tot
         
         return r2
-
+    
+    
+    # Metrics retrieval function
     def get_metrics(self) -> list:
-        return [self.R2]
+        return [self.R_Correlation, self.R2_Score]
     
     
+
+# %% ../nbs/metrics.ipynb 34
+class AccuracyMetrics(Metrics):
+    def __init__(self):
+        super().__init__()
+
+
+    # Metrics
+    def sMAPE(self, y_true, y_pred):
+        """
+        Calculate the Symmetric Mean Absolute Percentage Error (sMAPE).
+        
+        Parameters:
+        y_true (torch.Tensor): Actual values tensor of shape (batch_size, variables, horizon)
+        y_pred (torch.Tensor): Predicted values tensor of the same shape as y_true
+        
+        Returns:
+        torch.Tensor: sMAPE value
+        """
+        epsilon = 1e-8  # small constant to prevent division by zero
+        abs_error = torch.abs(y_true - y_pred)
+        symetric_error = ((torch.abs(y_true) + torch.abs(y_pred)) / 2.0) + epsilon
+        
+        smape = torch.mean(abs_error / symetric_error) * 100
+        return smape
+    
+    def MSA(self, y_true, y_pred):
+        """
+        Calculate the Mean Scaled Absolute Error (MSA).
+        
+        Parameters:
+        y_true (torch.Tensor): Actual values tensor of shape (batch_size, variables, horizon)
+        y_pred (torch.Tensor): Predicted values tensor of the same shape as y_true
+        
+        Returns:
+        torch.Tensor: MSA value
+        """
+        # Calculate the natural logarithm of the ratio
+        log_ratio = torch.abs(torch.log(y_pred / y_true))
+
+        msa = (torch.exp(torch.median(log_ratio)) - 1) * 100
+        
+        return msa
+    
+
+    # Metrics retrieval function
+    def get_metrics(self) -> list:
+        return [self.sMAPE, self.MSA]
+
+
+# %% ../nbs/metrics.ipynb 37
+class BiasMetrics(Metrics):
+    def __init__(self):
+        super().__init__()
+
+    # Metrics
+    def SSPB(self, y_true, y_pred):
+        """
+        Calculate the Symmetric Signed Percentage Bias (SSPB).
+        
+        Parameters:
+        y_true (torch.Tensor): Actual values tensor of shape (batch_size, variables, horizon)
+        y_pred (torch.Tensor): Predicted values tensor of the same shape as y_true
+        
+        Returns:
+        torch.Tensor: SSPB value
+        """
+        log_ratio = torch.log(y_pred / y_true)
+        median_log_ratio = torch.median(log_ratio)
+
+        sign = torch.sign(median_log_ratio)
+        
+        return sign * (torch.exp(torch.abs(median_log_ratio)) - 1) * 100
+
+
+    # Metrics retrieval function
+    def get_metrics(self) -> list:
+        return [self.SSPB]
